8a6a6870e9bb03a512aecc435f3615ecd13ebf58
Leonard4604 <leonardomaria.c@gmail.com>
2022-09-27 22:21:53 +0200

    Updated logger

--- modules/libs/logger/logger.js
+++ modules/libs/logger/logger.js
@@ -15,6 +15,9 @@ const logger = {
                       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg>
               </div>
+              <div id="clear-cart-volt-logger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"/><circle cx="20" cy="21" r="1"/><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"/></svg>
+              </div>
             </div>
         </div>
         <div id="volt-logger-content">
